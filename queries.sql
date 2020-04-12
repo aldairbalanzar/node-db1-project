addc1d4ef792ac16ad5b30cf1dfbba94731a607f
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-04-12 09:26:48 -0400

    updated to stretch

--- queries.sql
+++ queries.sql
@@ -28,5 +28,8 @@ set PostalCode = '11122'
 where ContactName = 'Bilbo Baggins';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
+SELECT count(distinct(city)) FROM Customers;
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+SELECT supplierName FROM [Suppliers]
+where length(supplierName)>20
